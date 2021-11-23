137ec62f0ce32186d36fe8e74ebf3519fcb7c788
carlossilva162 <carlos.silva16@estudante.ufla.br>
2021-11-22 23:46:59 -0300

    card de transaçoes

--- src/sreens/Resume/index.tsx
+++ src/sreens/Resume/index.tsx
@@ -1,16 +1,85 @@
-import React from 'react';
-import { ToastAndroid } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { HistoryCar } from '../../components/HistoryCar';
 
-import { Container, Header, Title } from './styles';
+import { Container, Header, Title, Content } from './styles';
+import { useFocusEffect } from '@react-navigation/core';
+import { categories } from '../../utils/categories';
+
+interface TransactionData {
+  type: 'positive' | 'negative';
+  name: string;
+  amount: string;
+  category: string;
+  date: string;
+}
+
+interface TotalByCategoryProps {
+  name: string;
+  total: string;
+  color: string;
+}
 
 export function Resume() {
+  const [totalByCategories, setTotalByCategories] = useState<
+    TotalByCategoryProps[]
+  >([]);
+
+  const loadData = async () => {
+    const collectionKey = '@gofinance:transactions';
+
+    const response = await AsyncStorage.getItem(collectionKey);
+
+    const responseFormatted = response ? JSON.parse(response) : [];
+    const expensives = responseFormatted.filter(
+      (expensive: TransactionData) => expensive.type === 'negative'
+    );
+
+    const totalByCategory = [] as TotalByCategoryProps[];
+
+    categories.forEach((category) => {
+      let categorySum = 0;
+
+      expensives.forEach((expensive: TransactionData) => {
+        if (category.name === expensive.category) {
+          categorySum += Number(expensive.amount);
+        }
+      });
+      if (categorySum > 0) {
+        const total = categorySum.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        });
+        totalByCategory.push({
+          name: category.name,
+          color: category.color,
+          total,
+        });
+      }
+    });
+    setTotalByCategories(totalByCategory);
+  };
+  useFocusEffect(
+    useCallback(() => {
+      loadData();
+    }, [])
+  );
+
   return (
     <Container>
       <Header>
         <Title>Resulmo por categoria</Title>
       </Header>
-      <HistoryCar title='compras' amount='Rs 150' color='red' />
+      <Content>
+        {totalByCategories.map((totalByCategory) => (
+          <HistoryCar
+            key={totalByCategory.name}
+            title={totalByCategory.name}
+            amount={totalByCategory.total}
+            color={totalByCategory.color}
+          />
+        ))}
+      </Content>
     </Container>
   );
 }
