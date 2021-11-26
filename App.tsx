6b571f8f2e2a39cc707e8343acd41341ce22bd32
carlossilva162 <carlos.silva16@estudante.ufla.br>
2021-11-25 22:09:24 -0300

    Ajust usecalback and loading, grafico,date ...

--- App.tsx
+++ App.tsx
@@ -10,7 +10,6 @@ import {
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
 import AppLoading from 'expo-app-loading';
-
 import theme from './src/global/styles/theme';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes/app.routes';
--- src/sreens/Resume/index.tsx
+++ src/sreens/Resume/index.tsx
@@ -1,10 +1,27 @@
 import React, { useCallback, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { HistoryCar } from '../../components/HistoryCar';
-
-import { Container, Header, Title, Content } from './styles';
-import { useFocusEffect } from '@react-navigation/core';
+import { VictoryPie } from 'victory-native';
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  ChatContainer,
+  MonthSelect,
+  MonthSelectButton,
+  MonthSelectIcon,
+  Month,
+} from './styles';
 import { categories } from '../../utils/categories';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useTheme } from 'styled-components';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import { addMonths, format, subMonths } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { LoadContainer } from '../Dashboard/styles';
+import { ActivityIndicator } from 'react-native';
+import { useFocusEffect } from '@react-navigation/core';
 
 interface TransactionData {
   type: 'positive' | 'negative';
@@ -16,7 +33,9 @@ interface TransactionData {
 
 interface TotalByCategoryProps {
   name: string;
-  total: string;
+  total: number;
+  totalFormatted: string;
+  percent: string;
   color: string;
 }
 
@@ -25,14 +44,45 @@ export function Resume() {
     TotalByCategoryProps[]
   >([]);
 
-  const loadData = async () => {
+  const [selectDate, setSelectDate] = useState(new Date());
+  const [isLoading, setIsLoading] = useState(false);
+
+  const theme = useTheme();
+
+  const handleChangeDate = useCallback(
+    (action: 'next' | 'prev') => {
+      if (action === 'next') {
+        const newDate = addMonths(selectDate, 1);
+        setSelectDate(newDate);
+      } else {
+        const newDate = subMonths(selectDate, 1);
+        setSelectDate(newDate);
+      }
+    },
+    [selectDate]
+  );
+
+  const loadData = useCallback(async () => {
+    setIsLoading(true);
+
     const collectionKey = '@gofinance:transactions';
 
     const response = await AsyncStorage.getItem(collectionKey);
 
     const responseFormatted = response ? JSON.parse(response) : [];
+
     const expensives = responseFormatted.filter(
-      (expensive: TransactionData) => expensive.type === 'negative'
+      (expensive: TransactionData) =>
+        expensive.type === 'negative' &&
+        new Date(expensive.date).getMonth() === selectDate.getMonth() &&
+        new Date(expensive.date).getFullYear() === selectDate.getFullYear()
+    );
+
+    const expensivesTotal = expensives.reduce(
+      (acumulator: number, currentValue: TransactionData) => {
+        return acumulator + Number(currentValue.amount);
+      },
+      0
     );
 
     const totalByCategory = [] as TotalByCategoryProps[];
@@ -46,23 +96,32 @@ export function Resume() {
         }
       });
       if (categorySum > 0) {
-        const total = categorySum.toLocaleString('pt-BR', {
+        const totalFormatted = categorySum.toLocaleString('pt-BR', {
           style: 'currency',
           currency: 'BRL',
         });
+
+        const percent = `${((categorySum / expensivesTotal) * 100).toFixed(
+          2
+        )}%`;
+
         totalByCategory.push({
           name: category.name,
           color: category.color,
-          total,
+          total: categorySum,
+          totalFormatted,
+          percent,
         });
       }
     });
     setTotalByCategories(totalByCategory);
-  };
+    setIsLoading(false);
+  }, [selectDate, totalByCategories]);
+
   useFocusEffect(
     useCallback(() => {
       loadData();
-    }, [])
+    }, [selectDate])
   );
 
   return (
@@ -70,16 +129,56 @@ export function Resume() {
       <Header>
         <Title>Resulmo por categoria</Title>
       </Header>
-      <Content>
-        {totalByCategories.map((totalByCategory) => (
-          <HistoryCar
-            key={totalByCategory.name}
-            title={totalByCategory.name}
-            amount={totalByCategory.total}
-            color={totalByCategory.color}
-          />
-        ))}
-      </Content>
+      {isLoading ? (
+        <LoadContainer>
+          <ActivityIndicator color={theme.colors.primary} size='large' />
+        </LoadContainer>
+      ) : (
+        <>
+          <MonthSelect>
+            <MonthSelectButton onPress={() => handleChangeDate('prev')}>
+              <MonthSelectIcon name='chevron-left' />
+            </MonthSelectButton>
+            <Month>{format(selectDate, 'MMMM ,yyyy', { locale: ptBR })}</Month>
+            <MonthSelectButton onPress={() => handleChangeDate('next')}>
+              <MonthSelectIcon name='chevron-right' />
+            </MonthSelectButton>
+          </MonthSelect>
+
+          <ChatContainer>
+            <VictoryPie
+              data={totalByCategories}
+              x='percent'
+              y='total'
+              colorScale={totalByCategories.map((category) => category.color)}
+              style={{
+                labels: {
+                  fontSize: RFValue(18),
+                  fontWeight: 'bold',
+                  fill: theme.colors.shape,
+                },
+              }}
+              labelRadius={50}
+            />
+          </ChatContainer>
+          <Content
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{
+              paddingHorizontal: 24,
+              paddingBottom: useBottomTabBarHeight(),
+            }}
+          >
+            {totalByCategories.map((totalByCategory) => (
+              <HistoryCar
+                key={totalByCategory.name}
+                title={totalByCategory.name}
+                amount={totalByCategory.totalFormatted}
+                color={totalByCategory.color}
+              />
+            ))}
+          </Content>
+        </>
+      )}
     </Container>
   );
 }
